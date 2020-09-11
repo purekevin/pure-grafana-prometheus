23ccd15d79d92da735357a97a3718df88a369c76
purekevin <60940499+purekevin@users.noreply.github.com>
2020-09-11 09:56:03 -0600

    Remove unneeded details about IP addresses.

--- install-crontab.sh
+++ install-crontab.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-INST_DIR=`pwd`
-DEFAULT_MANAGEMENT_IP="10.21.225.35"
-DEFAULT_NFS_VIP="10.21.225.38"
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-
 
 printf "${GREEN}-----------------  Installing crontab ------------------------${NC}\n"
 wget -q https://raw.githubusercontent.com/purekevin/dashboard/master/crontab
